efb3bf8c52e764dff8c729829332eb46d8a03123
Ivan Bilenkyi <ivanbilenkyi@gmail.com>
2023-11-06 23:02:16 +0200

    create tast

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,14 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { Layout } from './Layout';
+import initialContacts from '../data.json';
 import { PhonebookForm } from './PhonebookForm/PhonebookForm';
-// import { ContactFilter } from './Contacts/ContactFilter';
 import { ContactList } from './Contacts/ContactList';
 import { GlobalStyle } from './GlobalStyle';
 
-// import { nanoid } from 'nanoid';
-
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: initialContacts,
     filter: '',
   };
 
@@ -52,6 +45,20 @@ export class App extends Component {
       };
     });
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { contacts, filter } = this.state;
